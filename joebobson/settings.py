8ba5fe748e88d3e0b2fcfbd49d6c0e6b2e6ae947
rjuppa <rjuppa@gmail.com>
2014-12-29 00:16:13 +0100

    sorting static files

--- joebobson/settings.py
+++ joebobson/settings.py
@@ -113,8 +113,13 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
